86c04116839a9bd5e02552e6078e313a2cdc36e7
ChadoNihi <chadoishere@gmail.com>
2017-04-07 01:45:23 +0300

    save

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+const MAX_NUM_BC = 20999999.9769, // https://en.bitcoin.it/wiki/Controlled_supply#Projected_Bitcoins_Long_Term
+      FINAL_DATE = new Date(2140, 0);
+
+document.addEventListener("DOMContentLoaded", function() {
+
+});
