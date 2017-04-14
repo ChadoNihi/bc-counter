cacee67dc27d42aeb47c6b240d1c76d0feb8009d
ChadoNihi <chadoishere@gmail.com>
2017-04-15 00:05:22 +0300

    save

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,22 @@
 const MAX_NUM_BC = 20999999.9769, // https://en.bitcoin.it/wiki/Controlled_supply#Projected_Bitcoins_Long_Term
-      FINAL_DATE = new Date(2140, 0);
+      FINAL_DATE = new Date(2140, 0),
+      API_URL_TOTAL_BC = 'https://blockchain.info/q/totalbc';
 
 document.addEventListener("DOMContentLoaded", function() {
-
+  callAjax(API_URL_TOTAL_BC, onTotalBcAvailable);
 });
+
+function onTotalBcAvailable(XMLHttpResp) {
+
+}
+
+function callAjax(url, cb, verb){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = ()=> {
+        if (xhr.readyState == 4 && xhr.status == 200){
+            cb(xhr.responseText);
+        }
+    }
+    xhr.open((verb || "GET"), url, true);
+    xhr.send();
+}
